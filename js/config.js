21c7fd54485551811af4278a4a3298f36b1d16e8
dassoah33 <dassoah.info@gmail.com>
2024-02-20 12:37:59 +0100

    x
    x

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '891377246721', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3t547kpaler13l3q6q1d3gsc2o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
